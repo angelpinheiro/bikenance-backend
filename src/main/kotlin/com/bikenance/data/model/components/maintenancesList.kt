f539bd1ddc1ea0a7aabd483d9fc98772ddee3bde
Anxo Pinheiro <anxo@anxops.com>
2023-07-18 14:15:38 +0200

    Update maintenance intervals

--- src/main/kotlin/com/bikenance/data/model/components/maintenancesList.kt
+++ src/main/kotlin/com/bikenance/data/model/components/maintenancesList.kt
@@ -1,5 +1,12 @@
 package com.bikenance.data.model.components
 
+/**
+ * Default list of maintenances. In future versions, this will be stored on the user profile and will be
+ * editable from the app, so users can adapt maintenance intervals according to their preferences
+ */
+
+//TODO: Add maintenance priority
+
 val defaultMaintenances = listOf(
     MaintenanceInfo(
         type = MaintenanceTypes.BRAKE_MAINTENANCE,
@@ -11,7 +18,7 @@ val defaultMaintenances = listOf(
     ),
     MaintenanceInfo(
         type = MaintenanceTypes.CABLES_AND_HOUSING_MAINTENANCE,
-        defaultFrequency = RevisionFrequency(5000, RevisionUnit.KILOMETERS),
+        defaultFrequency = RevisionFrequency(6000, RevisionUnit.KILOMETERS),
     ),
     MaintenanceInfo(
         type = MaintenanceTypes.CASSETTE_MAINTENANCE,
@@ -23,15 +30,15 @@ val defaultMaintenances = listOf(
     ),
     MaintenanceInfo(
         type = MaintenanceTypes.CHAIN_MAINTENANCE,
-        defaultFrequency = RevisionFrequency(1000, RevisionUnit.KILOMETERS),
+        defaultFrequency = RevisionFrequency(1200, RevisionUnit.KILOMETERS),
     ),
     MaintenanceInfo(
         type = MaintenanceTypes.DISC_BRAKE_MAINTENANCE,
-        defaultFrequency = RevisionFrequency(5000, RevisionUnit.KILOMETERS),
+        defaultFrequency = RevisionFrequency(6000, RevisionUnit.KILOMETERS),
     ),
     MaintenanceInfo(
         type = MaintenanceTypes.DROPPER_POST_MAINTENANCE,
-        defaultFrequency = RevisionFrequency(5000, RevisionUnit.KILOMETERS),
+        defaultFrequency = RevisionFrequency(6000, RevisionUnit.KILOMETERS),
     ),
     MaintenanceInfo(
         type = MaintenanceTypes.FORK_MAINTENANCE,
@@ -39,11 +46,11 @@ val defaultMaintenances = listOf(
     ),
     MaintenanceInfo(
         type = MaintenanceTypes.FRONT_HUB_MAINTENANCE,
-        defaultFrequency = RevisionFrequency(6000, RevisionUnit.KILOMETERS),
+        defaultFrequency = RevisionFrequency(5000, RevisionUnit.KILOMETERS),
     ),
     MaintenanceInfo(
         type = MaintenanceTypes.REAR_HUB_MAINTENANCE,
-        defaultFrequency = RevisionFrequency(6000, RevisionUnit.KILOMETERS),
+        defaultFrequency = RevisionFrequency(5000, RevisionUnit.KILOMETERS),
     ),
     MaintenanceInfo(
         type = MaintenanceTypes.REAR_SUSPENSION_MAINTENANCE,
@@ -51,15 +58,15 @@ val defaultMaintenances = listOf(
     ),
     MaintenanceInfo(
         type = MaintenanceTypes.FRAME_BEARINGS_MAINTENANCE,
-        defaultFrequency = RevisionFrequency(6000, RevisionUnit.KILOMETERS),
+        defaultFrequency = RevisionFrequency(4000, RevisionUnit.KILOMETERS),
     ),
     MaintenanceInfo(
         type = MaintenanceTypes.THRU_AXLE_MAINTENANCE,
-        defaultFrequency = RevisionFrequency(5000, RevisionUnit.KILOMETERS),
+        defaultFrequency = RevisionFrequency(3000, RevisionUnit.KILOMETERS),
     ),
     MaintenanceInfo(
         type = MaintenanceTypes.TIRE_MAINTENANCE,
-        defaultFrequency = RevisionFrequency(1000, RevisionUnit.KILOMETERS),
+        defaultFrequency = RevisionFrequency(3000, RevisionUnit.KILOMETERS),
     ),
     MaintenanceInfo(
         type = MaintenanceTypes.WHEELSET_TUBELESS_MAINTENANCE,
@@ -67,6 +74,6 @@ val defaultMaintenances = listOf(
     ),
     MaintenanceInfo(
         type = MaintenanceTypes.WHEELSET_WHEELS_AND_SPOKES_MAINTENANCE,
-        defaultFrequency = RevisionFrequency(1000, RevisionUnit.KILOMETERS),
+        defaultFrequency = RevisionFrequency(2000, RevisionUnit.KILOMETERS),
     )
 )
